64eff46c2dbe0dfe77ede81e806f1daa43ea25f5
GomuGagan <gagangomu@gmail.com>
2022-10-13 15:34:59 +0530

    view work button link fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
@@ -12,7 +13,8 @@ const Home = () => {
         <h2 className='text-4xl sm:text-6xl font-bold text-[#8892b0]'>I'm a Frontend Developer.</h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a front-end developer specializing in building ( and occasionally designing) exceptional digital experience. Currently, I'm focused on building responsive web-applications.</p>
       <div>
-        
+      <Link to="work" smooth={true} duration={500}>
+          
         <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 group'>
           View Work 
           <span className="group-hover:rotate-90 duration-300" >
@@ -20,6 +22,7 @@ const Home = () => {
 
           </span>
           </button>
+        </Link>
       </div>
       </div>
 
