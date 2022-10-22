676074b01f1401f31c0f865b6a70a56e63e4fba5
GomuGagan <gagangomu@gmail.com>
2022-10-22 23:58:24 +0530

    comment error fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <form method="POST" action="https://getform.io/f/5c5c1920-daab-4c7e-9875-a59b0a2366d9" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold border-b-4 inline border-pink-600 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>// Submit the form Below or shoot me an E-mail:- gagangomu@gmail.com </p>
+                <p className='text-gray-300 py-4'> Submit the form Below or shoot me an E-mail:- gagangomu@gmail.com </p>
             </div>
             <input className="p-2 bg-[#ccd6f6]" type="text" placeholder='Name' name='name' />
             <input className='p-2 my-4 bg-[#ccd6f6]' type="email" placeholder='E-mail' name='email' />
